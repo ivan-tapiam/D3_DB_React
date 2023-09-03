8c88359c866789a8edab8122186254e9d47d2de1
ivan-tapiam <ivan.tapiam@mayor.cl>
2023-09-03 20:08:56 -0300

    Cargando desde cero

---
+++ desafio_BD/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Alert from './components/Alert'
+import Buscador from './components/Buscador'
+import Formulario from './components/Formulario'
+import Listado from './components/Listado'
+
+function App() {
+
+  return (
+    <>
+    <Alert />
+    <Buscador />
+    <Listado />
+    <Formulario />
+
+    </>
+  )
+}
+
+export default App
